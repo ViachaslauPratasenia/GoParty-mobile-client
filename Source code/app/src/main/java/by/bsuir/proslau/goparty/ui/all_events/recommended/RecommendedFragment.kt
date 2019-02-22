75cf3f72713a8f47c329ab59d9f90675463be0ee
ViachaslauPratasenia <slavaprot14@gmail.com>
2019-02-22 12:00:38 +0300

    Added floating button for add event

--- Source code/app/src/main/java/by/bsuir/proslau/goparty/ui/all_events/recommended/RecommendedFragment.kt
+++ Source code/app/src/main/java/by/bsuir/proslau/goparty/ui/all_events/recommended/RecommendedFragment.kt
@@ -7,6 +7,7 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import by.bsuir.proslau.goparty.R
 import by.bsuir.proslau.goparty.logic.events.EventLogic.Companion.initEvents
 import by.bsuir.proslau.goparty.ui.all_events.Event
@@ -19,6 +20,10 @@ class RecommendedFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         view = inflater.inflate(R.layout.fragment_recommended, container, false)
+        val fab = view.findViewById<View>(R.id.recommended_fab)
+        fab.setOnClickListener {
+            Toast.makeText(context, "Fab", Toast.LENGTH_SHORT).show()
+        }
         initImageBitmaps()
         return view
     }
