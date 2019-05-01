6ee19bb8d7d4eda1a4e98b8fb6d23bd21daa3650
ViachaslauPratasenia <slavaprot14@gmail.com>
2019-05-02 02:35:10 +0300

    Added user registration

--- Source code/app/src/main/java/by/bsuir/proslau/goparty/ui/authorization/PasswordActivity.kt
+++ Source code/app/src/main/java/by/bsuir/proslau/goparty/ui/authorization/PasswordActivity.kt
@@ -3,19 +3,40 @@ package by.bsuir.proslau.goparty.ui.authorization
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import by.bsuir.proslau.goparty.App
 import by.bsuir.proslau.goparty.MainActivity
 import by.bsuir.proslau.goparty.R
+import by.bsuir.proslau.goparty.db.local.LocalStore
+import by.bsuir.proslau.goparty.db.local.UserRepository
+import by.bsuir.proslau.goparty.entity.local.UserLocal
 import kotlinx.android.synthetic.main.activity_password.*
 
 class PasswordActivity : AppCompatActivity() {
+    private val localStore = LocalStore()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_password)
 
+        val newUser = intent.getSerializableExtra("newUser") as UserLocal
+
         btn_password_create.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+            if(checkConfirmation()){
+                newUser.password = input_password.text.toString()
+                UserRepository(App.context).create(newUser)
+                localStore.saveUserId(newUser.id)
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            } else{
+                Toast.makeText(App.context, "Passwords entered are different", Toast.LENGTH_SHORT).show()
+            }
+
         }
+    }
+
+    fun checkConfirmation(): Boolean{
+        if(input_password.text.toString().equals(input_confirm_password.text.toString())) return true
+        return false
     }
 }
